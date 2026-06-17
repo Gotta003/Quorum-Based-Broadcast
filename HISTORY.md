1064735ea5722490840bed3487184ad98082ef1d
Gotta003 <matteogottardelli@gmail.com>
2026-06-17 12:57:03 +0200

    Starting Replica.java impelmentation with heartbeat and internal token + initialization

--- HISTORY.md
+++ HISTORY.md
@@ -5,7 +5,5 @@ Matteo
 - [x] Define message classes: 
     - [x] Client Messages to communicate: ClientRead, ClientWrite, ReadReply, WriteReply (implemented in `ClientMessages.java`)
     - [x] Protocol Messages to coordinate: WriteForward, Update, Ack, WriteOK (implemented in `ProtocolMessages.java`)
-- [ ] Define: Update(epoch, seq, index, value, clientRef, originReplica), Ack(epoch, seq), WriteOK(epoch, seq)
-- [ ] Define: Heartbeat, HeartbeatTimeoutCheck, WriteForwardTimeout
-- [ ] Implement Replica.initSystem() -> store group map, coordinatorId, and init int[] positions with MAX LENGTH and init of epoch and seq to 0
-- [ ] Implement getSystemNumberOfActors()
+- [x] Define: Heartbeat, HeartbeatTimeoutCheck (as check by replicas), WriteForwardTimeout (timeout during WRITE_FORWARD) (`Replica.java`)
+- [x] Implement Replica.initSystem() -> store group map, coordinatorId, and init int[] positions with MAX LENGTH and init of epoch and seq to 0 and getSystemNumberOfActors(), returning the actors in the system (`Replica.java`)
--- src/main/java/it/unitn/ds/Replica.java
+++ src/main/java/it/unitn/ds/Replica.java
@@ -4,9 +4,17 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class Replica extends AbstractReplica {
+    //Variables
+    private int epoch;
+    private int seq;
+    private int[] positions;
+    private int currentCoordinatorId;
+    private Map<Integer, ActorRef> systemGroup;
     //Constructors
     public Replica(int id) {
         this(id, AbstractReplica.MIN_LATENCY, AbstractReplica.MAX_LATENCY, AbstractReplica.COORDINATOR_BEAT_INTERVAL, Optional.empty());
@@ -26,9 +34,37 @@ public class Replica extends AbstractReplica {
         return Props.create(Replica.class, () -> new Replica(id, minLatency, maxLatency, coordinatorBeatInterval, Optional.ofNullable(listener)));
     }
 
+    /*HEARTBEAT SIGNAL IMPLEMENTATION for control + timeout*/
+    /*Network message sent periodically by coordinator to all cohort replicas*/
+    public static final class Heartbeat implements Serializable {
+        public final int coordinatorId;
+        public Heartbeat(int coordinatorId) {
+            this.coordinatorId=coordinatorId;
+        }
+    }
+
+    /*Internal token as a local timer trigger. Replicas will schedule this message periodically via Scheduler to check if coordinator heartbeat has timed out*/
+    public static final class HeartbeatTimeoutCheck implements Serializable {
+        //Used only as signaling
+    }
+
+    /*Internal token scheduled by a replica after a WRITE_FORWARD. If UPDATE broadcast is not received within the timeout window, this message should trigger crash routine*/
+    public static final class WriteForwardTimeout implements Serializable {
+        public final int index;
+        public final int value;
+        public final ActorRef clientRef;
+        public WriteForwardTimeout(int index, int value, ActorRef clientRef) {
+            this.index=index;
+            this.value=value;
+            this.clientRef=clientRef;
+        }
+    }
+
     @Override
     public int getSystemNumberOfActors() {
-        // TODO: implement
+        if (this.systemGroup!=null) {
+            return this.systemGroup.size();
+        }
         return 0;
     }
 
@@ -39,7 +75,15 @@ public class Replica extends AbstractReplica {
 
     @Override
     public void initSystem(InitSystem sysInit) {
-        // TODO: implement
+        this.systemGroup=new HashMap<>(sysInit.group);
+        this.currentCoordinatorId=sysInit.coordinator_id;
+        this.positions=new int[POSITIONS_LIST_LENGTH];
+        for(int i=0; i<POSITIONS_LIST_LENGTH; i++) {
+            this.positions[i]=0;
+        }
+        this.epoch=0;
+        this.seq=0;
+        System.out.println("Replica " + this.id + " started correctly. Current Coordinator ID: " + this.currentCoordinatorId);
     }
 
     @Override
